429b864fc1b1fa7a4984836085c080ab5e59a072
dspamer <dspamer@e-xact.com>
2016-10-18 18:31:24 -0700

    Adding fir View and First Data in MongoDB

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const jade = require('jade')
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Constructing Classes/Models as well as passing it through a router:
+app.get('/',(req, res, next) => {
+  mongoose.model('User').find({}, (err, users) => {
+    if (err) {return next(err); }
+    res.render('index.jade', { users });
+  }); 
+});
+
+// For creating my DB Schema:
+mongoose.model('User', {
+  name: String
+});
+
+mongoose.connect('mongodb://localhost/finstagram', (err) => {
+  if (err) { throw err; }
+  app.listen(8000);
+  console.log('Ready on Port:8000')
+});
